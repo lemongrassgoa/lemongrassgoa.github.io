eabd1db6eee55cfe62f4c94353d1d3624626f1ec
ohnoitsalobo <alobo001@odu.edu>
2022-02-16 10:11:21 +0530

    update

--- list/js/csvToTable.js
+++ list/js/csvToTable.js
@@ -68,7 +68,7 @@
 	function buildTable() {
 		getCSV.call(this).then(function(response){
 			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
-	        var table = '<table class="sortable">';
+	        var table = '<table class="sortable" id="song_table">';
 	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
 	            if (singleRow === 0) {
 	                table += '<thead>';
