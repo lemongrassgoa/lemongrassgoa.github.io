d768af79a4119793c12615f2efce2a4871d35760
ohnoitsalobo <alobo001@odu.edu>
2023-01-22 21:01:31 +0530

    update

---
+++ list/wedding/js/csvToTable.js
@@ -0,0 +1,124 @@
+
+(function(){
+
+	// Constructor method
+	this.CsvToTable = function(){
+		this.csvFile = null;
+
+		// Create options by extending defaults with the passed in arugments
+    	if (arguments[0] && typeof arguments[0] === "object") {
+      		this.options = arguments[0];
+    	}
+
+	}
+
+	CsvToTable.prototype.run = function() {
+		return buildTable.call(this);
+	}
+
+	function getCSV() {
+		try{
+			var csvfile = this.options.csvFile;
+			return new Promise(function(resolve, reject) {
+				var request = new XMLHttpRequest();
+				request.open("GET", csvfile, true);
+				request.onload = function() {
+				    if (request.status == 200) {
+				        resolve(request.response);
+				    } else {
+				        reject(Error(request.statusText));
+				    }
+				};
+
+				request.onerror = function() {
+				 	reject(Error('Error fetching data.'));
+				};
+				request.send();
+			});
+		}catch(err){
+			console.error(err);
+		}
+	}
+
+    function isNotEmpty(row) {
+        return row !== "";
+    }
+
+    // polyfill `.filter()` for ECMAScript <5.1
+    // `f` must be pure (not modify original array).
+    if (!Array.prototype.filter) {
+      Array.prototype.filter = function(f) {
+        "use strict";
+        var p = arguments[1];
+        var o = Object(this);
+        var len = o.length;
+        for (var i = 0; i < len; i++) {
+          if (i in o) {
+              var v = o[i];
+              f.call(p, v, i, o);
+          }
+        }
+
+        return this;
+      };
+    }
+
+	function buildTable() {
+		getCSV.call(this).then(function(response){
+			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
+	        var table = '<table class=" sortable" id="repertoirelist">';
+	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+                var wip = 0;
+	            if (singleRow === 0) {
+	                table += '<thead class="tooltip-sort">';
+	            }
+                table += '<tr>';
+                    
+	            var rowCells = allRows[singleRow].split(',');
+	            for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
+	                if(singleRow === 0){
+                        if(rowCell < 9){
+                            table += '<th style="' + ((rowCell === 4) ? 'display:none;' : '') + '" class="' + ((rowCell === 0) ? ' dir-u ' : '') + '">';
+                            table += rowCells[rowCell];
+                            table += '</th>';
+                        }
+	                } else {
+                        if(rowCell < 9){
+                            var wip_format = 'background-color: #000;';
+                            if(rowCell === 0){
+                                table += '<td class="title" style="' + (rowCells[9] ? wip_format : '') + '">';
+
+                                table += '<i><a style="text-decoration: none;" target="_blank" href="http://localhost:81/'+ rowCells[10] + '">';
+                                table += rowCells[rowCell];
+                                table += '</a></i>'
+                            }
+                            else {
+                                table += '<td style="' + (rowCells[9] ? wip_format : '') + ((rowCell === 4) ? 'display:none;' : '') + '">'; 
+                                
+                                table += rowCells[rowCell];
+                                
+                                table += '</td>';
+                            }
+                        }
+	                }
+	            }
+	            if (singleRow === 0) {
+	                table += '</tr>';
+	                table += '</thead>';
+	                table += '<tbody class="list">';
+	            } else {
+	                table += '</tr>';
+	            }
+                
+	        }
+	        table += '</tbody>';
+	        table += '</table>';
+            
+            document.getElementById("repertoire-list").innerHTML += table;
+            
+        
+	}, function(error){
+			console.error(error);
+		});
+	}
+}());
---
+++ list/wedding/js/script.js
@@ -0,0 +1,55 @@
+var csvtotable = new CsvToTable({
+    csvFile: 'wedding.csv' 
+});
+csvtotable.run();
+
+function search(){
+    // Declare variables
+    var input, filter, table, tr, td0, td1, i, txtValue0, txtValue1;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("repertoirelist");
+    tr = table.getElementsByTagName("tr");
+    
+    var results = tr.length-1;
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td0 = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        if (td0 || td1) {
+            txtValue0 = td0.textContent || td0.innerText;
+            txtValue1 = td1.textContent || td1.innerText;
+            if (txtValue0.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else if (txtValue1.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+                results--;
+            }
+        }
+    }
+    document.getElementById("clear-search").innerHTML = results + " song" + (results == 1 ? "" : "s");
+}
+
+function clearSearch(){
+    document.getElementById("search").value = "";
+    search();
+}
+
+// THE //
+// Archies
+// Avett Brothers
+// Beatles
+// Carpenters
+// Cranberries
+// Doobie Brothers
+// Everly Brothers
+// Killers
+// Mayries
+// Monkees
+// Rembrandts
+// White Stripes
