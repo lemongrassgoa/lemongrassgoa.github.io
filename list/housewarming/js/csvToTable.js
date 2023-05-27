9157f95f50af1ff23c34fe879c5b1d17147dd480
ohnoitsalobo <alobo001@odu.edu>
2023-05-27 17:44:17 +0530

    update

--- list/housewarming/js/csvToTable.js
+++ list/housewarming/js/csvToTable.js
@@ -78,7 +78,12 @@
 	            for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
 	                if(singleRow === 0){
                         if(rowCell < 9){
-                            table += '<th style="' + ((rowCell === 4) ? 'display:none;' : '') + '" class="' + ((rowCell === 0) ? '' : '') + '">';
+                            table += '<th style="" class="' + ((rowCell === 0) ? ' dir-u ' : '') + '">';
+                            table += rowCells[rowCell];
+                            table += '</th>';
+                        }
+                        if(rowCell === 11){
+                            table += '<th class="tags">';
                             table += rowCells[rowCell];
                             table += '</th>';
                         }
@@ -86,19 +91,24 @@
                         if(rowCell < 9){
                             var wip_format = 'background-color: #000;';
                             if(rowCell === 0){
-                                table += '<td class="title" style="' + (rowCells[9] ? wip_format : '') + '">';
+                                table += '<td class="title' + (rowCells[9] ? ' wip' : '') + '" style="' + (rowCells[9] ? wip_format : '') + '">';
 
                                 table += '<i><a style="text-decoration: none;" target="_blank" href="http://localhost:81/'+ rowCells[10] + '">';
                                 table += rowCells[rowCell];
                                 table += '</a></i>'
                             }
                             else {
-                                table += '<td style="' + (rowCells[9] ? wip_format : '') + ((rowCell === 4) ? 'display:none;' : '') + '">'; 
+                                table += '<td style="' + (rowCells[9] ? wip_format : '') + '">'; 
                                 
                                 table += rowCells[rowCell];
                                 
                                 table += '</td>';
                             }
+                        }
+                        if(rowCell === 11){
+                            table += '<td class="tags" style="width: 5em; font-size: 80%; word-wrap: break-word;">';
+                            table += rowCells[rowCell];
+                            table += '</td>';
                         }
 	                }
 	            }
@@ -114,7 +124,40 @@
 	        table += '</tbody>';
 	        table += '</table>';
             
-            document.getElementById("repertoire-list").innerHTML += table;
+            // for(var i = 0; i < 3; i++){
+                // table += '<tr>';
+                // table += '<td colspan="8"></td>';
+                // table += '</tr>';
+            // }
+	        var table2 = '<br /><br /><table style="width: 70%; text-align: center;">';
+            table2 += '<tr> <td colspan="2"> HINDI SONGS </td> </tr>';
+            table2 += '<tr> <td colspan="2"><br /> </td> </tr>';
+            // table2 += '<tr> <td style="width: 25%;"> NAME </td> <td> LINK </td> </tr>';
+            table2 += '<tr> <td> Allah Ke Bande </td> </tr>';
+            table2 += '<tr> <td> Bang Bang </td> </tr>';
+            table2 += '<tr> <td> Darling </td> </tr>';
+            table2 += '<tr> <td> Dekha Hai Aise Bhi </td> </tr>';
+            table2 += '<tr> <td> Dilko Tumse Pyar Hua </td> </tr>';
+            table2 += '<tr> <td> Dooba Dooba </td> </tr>';
+            table2 += '<tr> <td> Gulabi Aankhen </td> </tr>';
+            table2 += '<tr> <td> Iktara </td> </tr>';
+            table2 += '<tr> <td> Kabira </td> </tr>';
+            table2 += '<tr> <td> Kahin To </td> </tr>';
+            table2 += '<tr> <td> Kaisi Paheli Zindagani </td> </tr>';
+            table2 += '<tr> <td> Khaabon Ke Parinday </td> </tr>';
+            table2 += '<tr> <td> Kyon </td> </tr>';
+            table2 += '<tr> <td> Main Kya Karoon </td> </tr>';
+            table2 += '<tr> <td> Oh Sanam </td> </tr>';
+            table2 += '<tr> <td> Pani Da Rang Male </td> </tr>';
+            table2 += '<tr> <td> Pyaar Ke Pal </td> </tr>';
+            table2 += '<tr> <td> Shaam </td> </tr>';
+            table2 += '<tr> <td> Sooraj Ki Baahon Mein </td> </tr>';
+            table2 += '<tr> <td> Tum Ho Toh </td> </tr>';
+            table2 += '<tr> <td> Yaaron Dosti </td> </tr>';
+            table2 += '<tr> <td> Ye Tumhari Meri Baatein </td> </tr>';
+            table2 += '</table>';
+
+            document.getElementById("repertoire-list").innerHTML += table + table2;
             
         
 	}, function(error){
