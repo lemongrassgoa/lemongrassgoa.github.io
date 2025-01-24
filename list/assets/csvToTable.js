2f256843ff2c44ef4dc15bbbdd258167b885bfc8
unknown <anandrlobo@gmail.com>
2025-01-24 10:33:11 +0530

    update

--- list/assets/csvToTable.js
+++ list/assets/csvToTable.js
@@ -13,6 +13,7 @@ const links_ = [
     ["Mamma Mia",                "P09oCbD8aJ8", 0],
     ["Norwegian Wood",           "iXiUKRlUfE4", 0],
     ["Perfect",                  "dSETYe7MeQ0", 0],
+    ["Price Tag",                "xkuX-nmXRLw", 0],
     ["Smooth Operator",          "nz85NleFjFs", 0],
     ["Snow",                     "R41hAQg_hXI", 0],
     ["Superman",                 "IJQls99Ut1s", 0],
