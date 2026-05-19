d358054b16bc1003b3bcdab557dbe0a36da11c08
ohnoitsalobo <anandrlobo@gmail.com>
2026-05-19 20:08:00 +0530

    update

--- list/assets/script.js
+++ list/assets/script.js
@@ -10,7 +10,7 @@ function onload(){
     setTimeout(function(){
         document.getElementById("search-results").style.opacity = "1";
         document.getElementById("repertoire-list").style.opacity = "1";
-    }, 300);
+    }, 30);
     setInterval(rainbowHeader, 25);
 }
   
